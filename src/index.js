f77933b0a6bf4217ec60e58b3966bc5a1fc5d6cf
Mike <michael83smith@gmail.com>
2026-06-01 13:43:23 -0400

    Add src/index.js

---
+++ src/index.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+const { connectRcon, startRconPolling } = require('./rcon/rconClient');
+const { registerCommands } = require('./discord/commands');
+const { handleInteraction } = require('./discord/interactionHandler');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.once('ready', async () => {
+  console.log(`✅ Logged in as ${client.user.tag}`);
+  await registerCommands(client);
+  await connectRcon(client);
+  startRconPolling(client);
+});
+
+client.on('interactionCreate', (interaction) => handleInteraction(interaction));
+
+client.login(process.env.DISCORD_TOKEN);
